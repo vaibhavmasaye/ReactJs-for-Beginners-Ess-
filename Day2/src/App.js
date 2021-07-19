bd81b82e8df0e702aa1883dea757ac628bae83e0
vaibhavmasaye <47131806+vaibhavmasaye@users.noreply.github.com>
2021-07-19 08:51:11 +0530

    Add files via upload

---
+++ Day2/src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import './App.css';
+
+const App = ()=>{
+
+    let [score, setScore] = useState(0)
+    
+    return(
+        <div>
+             <h1> Hey this is my Counter Application </h1>
+             <h3>The value of score is {score}</h3>
+             <button onClick={()=>(score<25)? setScore(score+1) : "" }> Increment </button>
+             <button onClick={()=>(score>0)? setScore(score-1) : "" }> Decrement </button>
+             <button onClick={()=>{setScore(0)}}> Reset </button>
+        </div>
+    )
+}
+
+export default App
