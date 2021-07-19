267f5a642f6a281849e80c7296189a4ed79e14f3
vaibhavmasaye <47131806+vaibhavmasaye@users.noreply.github.com>
2021-07-19 08:26:47 +0530

    Add files via upload

---
+++ Day1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1 style={{ textAlign : "center"}}> Hello world </h1>
+       <hr></hr>
+    </div>
+  );
+}
+
+export default App;
